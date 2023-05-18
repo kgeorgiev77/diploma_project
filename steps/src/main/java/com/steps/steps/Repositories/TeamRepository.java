3b8dfbc07cd66838fd940c709dfda93134edcab8
kgeorgiev77 <48562595+kgeorgiev77@users.noreply.github.com>
2023-05-18 10:58:38 +0300

    Delete steps directory

--- steps/src/main/java/com/steps/steps/Repositories/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.steps.steps.Repositories;
-
-import com.steps.steps.Entities.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
